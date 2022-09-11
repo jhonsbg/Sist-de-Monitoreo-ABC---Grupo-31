f91ec25d5131a8b8c198af07345cf06837a907d6
Sebastian Bohorquez <sebastian.bohorquez@ariadnacg.com>
2022-09-11 13:45:27 -0500

    Microservicio Monitoreo

--- microservicio_monitor/app.py
+++ microservicio_monitor/app.py
@@ -16,24 +16,23 @@ api.init_app(app)
 class Monitor(Resource):
 
     def post(self):
-        content = requests.get('http://127.0.0.1:5001/sensor')        
         
-        if content.status_code == 404:
-            return content.json(),404
-        else:
+        mensaje_respuesta = ""
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S") 
+        filename = 'historico_sensor_monitoreo.csv'
+
+        try: 
+            content = requests.get('http://127.0.0.1:5001/sensor')        
+            
             sensor = content.json()
-            sensor["Estado"] = request.json["Estado"]
             respuesta_string = str(sensor["Estado"])
             args = (sensor,)
-            now = datetime.now()
-            dt_string = now.strftime("%d/%m/%Y %H:%M:%S") 
-            filename = 'historico_sensor_monitoreo.csv'
             with open(filename, 'a', newline="") as file:
-               file.write(dt_string)
-               file.write(",") 
-               file.write(respuesta_string)
-               file.write("\n")
-            mensaje_respuesta = ""
+                file.write(dt_string)
+                file.write(",") 
+                file.write(respuesta_string)
+                file.write("\n")
             if respuesta_string == False:
                 mensaje_respuesta = 'Sensor fuera de servicio'
             elif respuesta_string == True:
@@ -41,6 +40,44 @@ class Monitor(Resource):
             else:
                 mensaje_respuesta = 'Sistema no disponible'            
             return json.dumps(mensaje_respuesta)    
-
+        # except requests.exceptions.sensornodisponible as errh : 
+        #     with open(filename, 'a', newline="") as file:
+        #         file.write(dt_string)
+        #         file.write(",") 
+        #         file.write("False")
+        #         file.write("\n")
+        #     return("Componente sensor no disponible")
+        except requests.exceptions.HTTPError as errh:
+            print ("Http Error: ",errh)
+            with open(filename, 'a', newline="") as file:
+                file.write(dt_string)
+                file.write(",") 
+                file.write("False")
+                file.write("\n")
+            return("Falla en la comunicación con componente Sensor")
+        except requests.exceptions.ConnectionError as errc:
+            print ("Error Connecting: ",errc)
+            with open(filename, 'a', newline="") as file:
+                file.write(dt_string)
+                file.write(",") 
+                file.write("False")
+                file.write("\n")
+            return("Falla en la comunicación con componente Sensor")
+        except requests.exceptions.Timeout as errt:
+            print ("Timeout Error: ",errt)
+            with open(filename, 'a', newline="") as file:
+                file.write(dt_string)
+                file.write(",") 
+                file.write("False")
+                file.write("\n")
+            return("Falla en la comunicación con componente Sensor")
+        except requests.exceptions.RequestException as err:
+            print ("Algo salio mal: ",err)
+            with open(filename, 'a', newline="") as file:
+                file.write(dt_string)
+                file.write(",") 
+                file.write("False")
+                file.write("\n")
+            return("Falla en la comunicación con componente Sensor")
     
 api.add_resource(Monitor, '/monitoreo')
