9c3574de985dc35243ada78c06d9a2c2e065eb3c
Krishtian Barrera <kd.barrera@uniandes.edu.co>
2022-10-02 22:09:50 -0500

    Modelo Usuario y JWT login

--- microservicio_seguridad/__init__.py
+++ microservicio_seguridad/__init__.py
@@ -3,4 +3,8 @@ from flask import Flask
 def create_app(config_name):
     app = Flask(__name__)
     app.config['JWT_SECRET_KEY']='frase-secreta'     
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///eporra.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
     return app
--- microservicio_seguridad/app.py
+++ microservicio_seguridad/app.py
@@ -5,25 +5,31 @@ import json
 import requests
 import random
 from datetime import datetime
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import JWTManager, get_jwt
 from flask_jwt_extended import jwt_required, create_access_token
+from .modelos import db, Usuario
 
 
 app = create_app('default')
 app_context = app.app_context()
 app_context.push()
+
+db.init_app(app)
+db.create_all()
 api = Api(app)
 api.init_app(app)
 jwt = JWTManager(app)
 
-class Login(Resource):
+class Login(Resource):          
 
-    def get(self):           
-        
-        respuesta_string = "Alarma Activa 1"        
-        return {'dashboard': respuesta_string} 
-
-
-
+    def post(self):
+        usuario = Usuario.query.filter(Usuario.usuario == request.json["usuario"],
+                                       Usuario.contrasena == request.json["contrasena"]).first()
+        db.session.commit()
+        if usuario is None:
+            return "El usuario no existe", 404
+        else:
+            token_de_acceso = create_access_token(identity=usuario.id, additional_claims = {"rol": usuario.tipoUsuario,"idUsuario": usuario.id})
+            return {"mensaje": "Inicio de sesión exitoso", "token": token_de_acceso}
 
 api.add_resource(Login, '/login')
