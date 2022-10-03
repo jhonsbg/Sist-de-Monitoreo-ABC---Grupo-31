490bacaba0bb967b87669d2a6dd06130c5b78cd2
Nelson <n.ortizf@uniandes.edu.co>
2022-10-02 21:51:59 -0500

    libreria JWT

--- microservicio_seguridad/app.py
+++ microservicio_seguridad/app.py
@@ -5,6 +5,8 @@ import json
 import requests
 import random
 from datetime import datetime
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import jwt_required, create_access_token
 
 
 app = create_app('default')
@@ -12,13 +14,16 @@ app_context = app.app_context()
 app_context.push()
 api = Api(app)
 api.init_app(app)
+jwt = JWTManager(app)
 
 class Login(Resource):
 
     def get(self):           
         
         respuesta_string = "Alarma Activa 1"        
-        return {'dashboard': respuesta_string}   
+        return {'dashboard': respuesta_string} 
+
+
 
 
 api.add_resource(Login, '/login')
