3c0c94bb0f9546a8ab117663525aaef723adc263
LuisCruz <lf.cruz2@uniandes.edu.co>
2022-10-02 22:31:03 -0500

    end point get con restircion de autenticacion

--- microservicio_seguridad/app.py
+++ microservicio_seguridad/app.py
@@ -9,7 +9,6 @@ from flask_jwt_extended import JWTManager, get_jwt
 from flask_jwt_extended import jwt_required, create_access_token
 from .modelos import db, Usuario
 
-
 app = create_app('default')
 app_context = app.app_context()
 app_context.push()
@@ -20,16 +19,30 @@ api = Api(app)
 api.init_app(app)
 jwt = JWTManager(app)
 
-class Login(Resource):          
+
+class Login(Resource):
+
+    @jwt_required()
+    def get(self):
+        claims = get_jwt()
+        if claims['Rol'] == 'Operador':
+            respuesta_string = 'Alarma Activa 1'
+            return {'dashboard': respuesta_string}
+        else:
+            respuesta_string = 'No posee permisos suficientes para ver esta notificación'
+            return {'dashboard': respuesta_string}
 
     def post(self):
+
         usuario = Usuario.query.filter(Usuario.usuario == request.json["usuario"],
                                        Usuario.contrasena == request.json["contrasena"]).first()
         db.session.commit()
         if usuario is None:
             return "El usuario no existe", 404
         else:
-            token_de_acceso = create_access_token(identity=usuario.id, additional_claims = {"rol": usuario.tipoUsuario,"idUsuario": usuario.id})
+            token_de_acceso = create_access_token(identity=usuario.id, additional_claims={"rol": usuario.tipoUsuario,
+                                                                                          "idUsuario": usuario.id})
             return {"mensaje": "Inicio de sesión exitoso", "token": token_de_acceso}
+
 
 api.add_resource(Login, '/login')
